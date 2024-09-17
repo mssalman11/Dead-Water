ed9dabe18217a4690723aa35d1811949f82886f0
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2024-09-17 11:56:04 -0700

    added script

--- DW (Dead Water)/Assets/Scripts/ElizeoScripts/BattleSystem.cs
+++ DW (Dead Water)/Assets/Scripts/ElizeoScripts/BattleSystem.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/* [Nava, Elizeo]
+ * [September 17, 2024]
+ * [Placeholder]
+ */
 public class BattleSystem : MonoBehaviour
 {
+    public enum Syatem
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
