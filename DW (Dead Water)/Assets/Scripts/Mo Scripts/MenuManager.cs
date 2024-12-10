98d1fd02864ef3c560d54a2ddf48e1208bf7c0d5
mssalman11 <mssalman@csuchico.edu>
2024-12-09 18:19:33 -0800

    Updated Main Menu

--- DW (Dead Water)/Assets/Scripts/Mo Scripts/MenuManager.cs
+++ DW (Dead Water)/Assets/Scripts/Mo Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,58 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public int instructionID = 0;
+
+    [SerializeField]
+    private GameObject instruct1, instruct2, instruct3, instruct4;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene("Level0Test");
     }
 
-    public void LoadShop()
+    public void nextButt()
     {
-        SceneManager.LoadScene("Shop");
+        instructionID++;
+    }
+    public void prevButt()
+    {
+        instructionID--;
     }
 
-    public void LoadCharacterMenu()
+    private void Update()
     {
-        SceneManager.LoadScene("Characters_Leland");
+        switch (instructionID)
+        {
+            case 1:
+                instruct1.SetActive(true); instruct2.SetActive(false); instruct3.SetActive(false); instruct4.SetActive(false);
+                break;
+            case 2:
+                instruct1.SetActive(false); instruct2.SetActive(true); instruct3.SetActive(false); instruct4.SetActive(false);
+                break;
+            case 3:
+                instruct1.SetActive(false); instruct2.SetActive(false); instruct3.SetActive(true); instruct4.SetActive(false);
+                break;
+            case 4:
+                instruct1.SetActive(false); instruct2.SetActive(false); instruct3.SetActive(false); instruct4.SetActive(true);
+                break;
+            default:
+                if (instructionID >= 5)
+                {
+                    instructionID = 4;
+                }
+                else if (instructionID < 0)
+                {
+                    instructionID = 1;
+                }
+                break;
+        }
     }
 
-    public void LoadMainMenu()
+    public void turnoffall()
     {
-        SceneManager.LoadScene("MainMenus");
+        instruct1.SetActive(false); instruct2.SetActive(false); instruct3.SetActive(false); instruct4.SetActive(false);
+        instructionID = 0;
     }
+
 }
